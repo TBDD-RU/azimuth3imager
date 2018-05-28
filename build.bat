5d481c3a6142cb5dfee3c934bcaa390817699ef3
alkorgun <alkorgun@gmail.com>
2018-05-28 07:47:10 +0500

    build files updated (the new environment variables required)

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@ mingw32-make
 cd ..
 mkdir build
 move azimuth3imager.exe build
+copy %LIBBOOST_LIBPATH%\%LIBBOOST_SYSTEM_BINARY_NAME%.dll build\
+copy %LIBBOOST_LIBPATH%\%LIBBOOST_FILESYSTEM_BINARY_NAME%.dll build\
 copy %LIBBOOST_LIBPATH%\%LIBBOOST_IOSTREAMS_BINARY_NAME%.dll build\
 copy %LIBBOOST_LIBPATH%\%LIBBOOST_BZIP2_BINARY_NAME%.dll build\
 cd build
